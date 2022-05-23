d1f5f2314ed9694c3fd12e23e80399c1bc5d6e3a
Harsual <onori273@gmail.com>
2022-05-23 03:43:53 +0300

    fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fac8d04657c0bd81af26"; }
+/******/ 		__webpack_require__.h = function() { return "9ea5079eec83cca16c91"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
