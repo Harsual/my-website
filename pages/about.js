df1804f5bf6820995e34cf95592a5a9969f0aa97
Osn132 <40571631+Harsual@users.noreply.github.com>
2024-02-12 00:13:32 -0330

    test

--- pages/about.js
+++ pages/about.js
@@ -6,51 +6,7 @@ import Image from "next/image";
 
 
 export default function about() {
-  /*document.addEventListener("DOMContentLoaded", function () {
-    if (!window.AnimationEvent) {
-      return;
-    }
-    var anchors = document.getElementsByTagName("a");
-
-    for (var idx = 0; idx < anchors.length; idx += 1) {
-      if (
-        anchors[idx].hostname !== window.location.hostname ||
-        anchors[idx].pathname === window.location.pathname
-      ) {
-        continue;
-      }
-    }
-
-    anchors[idx].addEventListener("click", function (event) {
-      var fader = document.getElementById("fader"),
-        anchor = event.currentTarget;
-
-      var listener = function () {
-        window.location = anchor.href;
-        fader.removeEventListener("animationend", listener);
-      };
-      fader.addEventListener("animationend", listener);
-
-      event.preventDefault();
-
-      fader.classList.add("fade-in");
-    });
-  });
-  window.addEventListener("pageshow", function (event) {
-    if (!event.persisted) {
-      return;
-    }
-    var fader = document.getElementById("fader");
-    fader.classList.remove("fade-in");
-  });
-
-  const fadeInPage = () => {
-    if (!window.AnimationEvent) {
-      return;
-    }
-    var fader = document.getElementById("fader");
-    fader.classList.add("fade-out");
-  };*/
+  
 
   return (
     <div className={styles.container}>
